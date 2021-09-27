d2d05c24b7ba8e97ec903812688f9d7be69b551e
Curve <fynnbwdt@gmail.com>
2021-09-27 20:07:13 +0200

    fix(tests): signature checks

--- tests/signature.test.cpp
+++ tests/signature.test.cpp
@@ -17,24 +17,24 @@ TEST_CASE("Signature scanning is tested", "[signature]")
     REQUIRE(this_module);
 
     auto ida_sig = lime::sig("?? C7 ? 39 05 00 00 48 2D 95 03 00 00");
-    CHECK(*ida_sig.find<false>() == reinterpret_cast<std::uintptr_t>(test) + 4);
-    CHECK(ida_sig.find<true>().front() == reinterpret_cast<std::uintptr_t>(test) + 4);
+    CHECK((*ida_sig.find<false>() - reinterpret_cast<std::uintptr_t>(test)) < 10);
+    CHECK((ida_sig.find<true>().front() - reinterpret_cast<std::uintptr_t>(test)) < 10);
 
-    CHECK(*ida_sig.find_in<false>(*test_page) == reinterpret_cast<std::uintptr_t>(test) + 4);
-    CHECK(ida_sig.find_in<true>(*test_page).front() == reinterpret_cast<std::uintptr_t>(test) + 4);
+    CHECK((*ida_sig.find_in<false>(*test_page) - reinterpret_cast<std::uintptr_t>(test)) < 10);
+    CHECK((ida_sig.find_in<true>(*test_page).front() - reinterpret_cast<std::uintptr_t>(test)) < 10);
 
-    CHECK(*ida_sig.find_in<false>(*this_module) == reinterpret_cast<std::uintptr_t>(test) + 4);
-    CHECK(ida_sig.find_in<true>(*this_module).front() == reinterpret_cast<std::uintptr_t>(test) + 4);
+    CHECK((*ida_sig.find_in<false>(*this_module) - reinterpret_cast<std::uintptr_t>(test)) < 10);
+    CHECK((ida_sig.find_in<true>(*this_module).front() - reinterpret_cast<std::uintptr_t>(test)) < 10);
 
     auto code_sig = lime::sig("\x00\xC7\x00\x39\x05\x00\x00\x48\x2D\x95\x03\x00\x00", "?x?xxxxxxxxxx");
-    CHECK(*code_sig.find<false>() == reinterpret_cast<std::uintptr_t>(test) + 4);
-    CHECK(code_sig.find<true>().front() == reinterpret_cast<std::uintptr_t>(test) + 4);
+    CHECK((*code_sig.find<false>() - reinterpret_cast<std::uintptr_t>(test)) < 10);
+    CHECK((code_sig.find<true>().front() - reinterpret_cast<std::uintptr_t>(test)) < 10);
 
-    CHECK(*code_sig.find_in<false>(*test_page) == reinterpret_cast<std::uintptr_t>(test) + 4);
-    CHECK(code_sig.find_in<true>(*test_page).front() == reinterpret_cast<std::uintptr_t>(test) + 4);
+    CHECK((*code_sig.find_in<false>(*test_page) - reinterpret_cast<std::uintptr_t>(test)) < 10);
+    CHECK((code_sig.find_in<true>(*test_page).front() - reinterpret_cast<std::uintptr_t>(test)) < 10);
 
-    CHECK(*code_sig.find_in<false>(*this_module) == reinterpret_cast<std::uintptr_t>(test) + 4);
-    CHECK(code_sig.find_in<true>(*this_module).front() == reinterpret_cast<std::uintptr_t>(test) + 4);
+    CHECK((*code_sig.find_in<false>(*this_module) - reinterpret_cast<std::uintptr_t>(test)) < 10);
+    CHECK((code_sig.find_in<true>(*this_module).front() - reinterpret_cast<std::uintptr_t>(test)) < 10);
 
     //
     CHECK(ida_sig.find<true>() == code_sig.find<true>());
