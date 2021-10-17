e92d2a71b2328c753b531f5c43065a9f76f30c51
Curve <fynnbwdt@gmail.com>
2021-10-17 15:48:52 +0200

    chore(tests): update keyboard test

--- tests/keyboard.test.cpp
+++ tests/keyboard.test.cpp
@@ -4,9 +4,13 @@
 
 TEST_CASE("Keyboard utility is tested", "[keyboard]")
 {
+    lime::keyboard::release(lime::key::escape);
+    REQUIRE(!lime::keyboard::is_down(lime::key::escape));
+
     lime::keyboard::press(lime::key::escape);
     REQUIRE(lime::keyboard::was_pressed(lime::key::escape));
     REQUIRE(!lime::keyboard::was_pressed(lime::key::escape));
+    REQUIRE(lime::keyboard::is_down(lime::key::escape));
 
     lime::keyboard::release(lime::key::escape);
     REQUIRE(!lime::keyboard::is_down(lime::key::escape));
