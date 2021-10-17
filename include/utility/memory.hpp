6a10cf2b2a5d89793b3b6b0a6f06bc13eca32df8
Curve <fynnbwdt@gmail.com>
2021-10-17 17:51:00 +0200

    refactor(memory): remove useless static

--- include/utility/memory.hpp
+++ include/utility/memory.hpp
@@ -6,10 +6,10 @@
 namespace lime
 {
     bool write(const std::uintptr_t &address, const void *data, const std::size_t &size);
-    template <class type_t> static bool write(const std::uintptr_t &address, const type_t &value);
+    template <class type_t> bool write(const std::uintptr_t &address, const type_t &value);
 
     std::unique_ptr<char[]> read(const std::uintptr_t &address, const std::size_t &size);
-    template <class type_t> static std::optional<type_t> read(const std::uintptr_t &address);
+    template <class type_t> std::optional<type_t> read(const std::uintptr_t &address);
 
     bool free(const std::uintptr_t &address, const std::size_t &size);
     bool protect(const std::uintptr_t &address, const std::size_t &size, const std::uint8_t &protection);
