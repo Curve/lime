8408a9a13b4bbc9a71fc80dbbc0229cf41eafed6
Curve <fynnbwdt@gmail.com>
2022-06-09 18:47:38 +0200

    feat: update entrypoint test

--- tests/entrypoint.test.cpp
+++ tests/entrypoint.test.cpp
@@ -1,10 +1,19 @@
 #include <catch2/catch.hpp>
 #include <utility/entrypoint.hpp>
 
-template <typename... T> void entry(const T &...) {}
-template <typename... T> void exit(const T &...) {}
+inline bool success = false;
+
+template <typename... T> void entry(const T &...)
+{
+    success = true;
+}
+
+template <typename... T> void quit(const T &...) {}
 
 TEST_CASE("Entrypoint code is tested", "[entrypoint]")
 {
-    //? This check just exists to check wether or not the entrypoint code compiles as we can't really test it here.
+//? This check just exists to check wether or not the entrypoint code compiles as we can't really test it here.
+#if defined(__linux__)
+    REQUIRE(success);
+#endif
 }
