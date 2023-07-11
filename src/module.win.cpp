936029f296bec8792115c1e015977761d44b1568
Curve <fynnbwdt@gmail.com>
2023-07-11 18:06:02 +0200

    feat: add windows support

---
+++ src/module.win.cpp
@@ -0,0 +1,197 @@
+#include "module.hpp"
+
+#include <windows.h>
+#include <imagehlp.h>
+#include <psapi.h>
+
+#include <functional>
+
+namespace lime
+{
+    struct module::impl
+    {
+        HMODULE handle;
+
+      public:
+        MODULEINFO info;
+        std::string name;
+
+      public:
+        void iterate_symbols(const std::function<bool(const std::string &)> &) const;
+    };
+
+    module::module() :m_impl(std::make_unique<impl>()) {}
+
+    module::~module() = default;
+
+    module::module(const module &other) :m_impl(std::make_unique<impl>())
+    {
+        *m_impl = *other.m_impl;
+    }
+
+    module::module(module &&other) noexcept :m_impl(std::move(other.m_impl)) {}
+
+    std::string module::name() const
+    {
+        return m_impl->name;
+    }
+
+    std::size_t module::size() const
+    {
+        return m_impl->info.SizeOfImage;
+    }
+
+    std::uintptr_t module::end() const
+    {
+        return start() + size();
+    }
+
+    std::uintptr_t module::start() const
+    {
+        return reinterpret_cast<std::uintptr_t>(m_impl->info.lpBaseOfDll);
+    }
+
+    std::vector<lime::symbol> module::symbols() const
+    {
+        std::vector<lime::symbol> rtn;
+
+        m_impl->iterate_symbols([&](const std::string &name) {
+            rtn.emplace_back(lime::symbol{.name = name, .address = symbol(name)});
+            return false;
+        });
+
+        return rtn;
+    }
+
+    std::uintptr_t module::symbol(const std::string &name) const
+    {
+        return reinterpret_cast<std::uintptr_t>(GetProcAddress(m_impl->handle, name.c_str()));
+    }
+
+    std::optional<std::uintptr_t> module::find_symbol(const std::string &name) const
+    {
+        std::uintptr_t rtn{0};
+
+        m_impl->iterate_symbols([&](const std::string &item) {
+            if (item.find(name) == std::string::npos)
+            {
+                return false;
+            }
+
+            rtn = symbol(item);
+            return true;
+        });
+
+        if (!rtn)
+        {
+            return std::nullopt;
+        }
+
+        return rtn;
+    }
+
+    std::vector<module> module::modules()
+    {
+        std::vector<module> rtn;
+
+        DWORD modules_size{};
+        HMODULE modules[1024];
+
+        if (!EnumProcessModules(GetCurrentProcess(), modules, sizeof(modules), &modules_size))
+        {
+            return rtn;
+        }
+
+        for (auto i = 0u; (modules_size / sizeof(HMODULE)) > i; i++)
+        {
+            CHAR name[MAX_PATH];
+
+            if (!GetModuleBaseNameA(GetCurrentProcess(), modules[i], name, MAX_PATH))
+            {
+                continue;
+            }
+
+            MODULEINFO info;
+
+            if (!GetModuleInformation(GetCurrentProcess(), modules[i], &info, sizeof(info)))
+            {
+                continue;
+            }
+
+            module item;
+
+            item.m_impl->name = name;
+            item.m_impl->info = info;
+            item.m_impl->handle = modules[i];
+
+            rtn.emplace_back(std::move(item));
+        }
+
+        return rtn;
+    }
+
+    std::optional<module> module::get(const std::string &name)
+    {
+        auto all = modules();
+        auto module = std::find_if(all.begin(), all.end(), [&](auto &item) { return item.name() == name; });
+
+        if (module == all.end())
+        {
+            return std::nullopt;
+        }
+
+        return std::move(*module);
+    }
+
+    std::optional<module> module::find(const std::string &name)
+    {
+        auto all = modules();
+
+        constexpr auto npos = std::string::npos;
+        auto module = std::find_if(all.begin(), all.end(), [&](auto &item) { return item.name().find(name) != npos; });
+
+        if (module == all.end())
+        {
+            return std::nullopt;
+        }
+
+        return std::move(*module);
+    }
+
+    void module::impl::iterate_symbols(const std::function<bool(const std::string &)> &callback) const
+    {
+        CHAR path[MAX_PATH];
+        GetModuleFileNameA(handle, path, MAX_PATH);
+
+        _LOADED_IMAGE image;
+        if (!MapAndLoad(path, nullptr, &image, TRUE, TRUE))
+        {
+            return;
+        }
+
+        ULONG size{};
+        const auto *export_directory = reinterpret_cast<_IMAGE_EXPORT_DIRECTORY *>(
+            ImageDirectoryEntryToData(image.MappedAddress, false, IMAGE_DIRECTORY_ENTRY_EXPORT, &size));
+
+        if (!export_directory)
+        {
+            return;
+        }
+
+        const auto *rvas = reinterpret_cast<DWORD *>(
+            ImageRvaToVa(image.FileHeader, image.MappedAddress, export_directory->AddressOfNames, nullptr));
+
+        for (size_t i = 0; export_directory->NumberOfNames > i; i++)
+        {
+            const auto *name =
+                reinterpret_cast<char *>(ImageRvaToVa(image.FileHeader, image.MappedAddress, rvas[i], nullptr));
+
+            if (callback(name))
+            {
+                break;
+            }
+        }
+
+        UnMapAndLoad(&image);
+    }
+} // namespace lime
---
+++ src/page.win.cpp
@@ -0,0 +1,252 @@
+#include "page.hpp"
+
+#include <limits>
+#include <windows.h>
+#include <memoryapi.h>
+
+namespace lime
+{
+    struct page::impl
+    {
+        protection prot;
+        DWORD original_prot;
+
+      public:
+        std::uintptr_t end;
+        std::uintptr_t start;
+
+      public:
+        static DWORD translate(protection);
+        static std::shared_ptr<page> from(LPVOID, std::size_t, protection);
+    };
+
+    page::page() : m_impl(std::make_unique<impl>()) {}
+
+    page::~page() = default;
+
+    page::page(const page &other) : m_impl(std::make_unique<impl>())
+    {
+        *m_impl = *other.m_impl;
+    }
+
+    page::page(page &&other) noexcept : m_impl(std::move(other.m_impl)) {}
+
+    page &page::operator=(page &&other) noexcept
+    {
+        m_impl = std::move(other.m_impl);
+        return *this;
+    }
+
+    protection page::prot() const
+    {
+        return m_impl->prot;
+    }
+
+    std::size_t page::size() const
+    {
+        return m_impl->end - m_impl->start;
+    }
+
+    std::uintptr_t page::end() const
+    {
+        return m_impl->end;
+    }
+
+    std::uintptr_t page::start() const
+    {
+        return m_impl->start;
+    }
+
+    bool page::restore()
+    {
+        if (VirtualProtect(reinterpret_cast<LPVOID>(start()), size(), m_impl->original_prot, nullptr) == 0)
+        {
+            return false;
+        }
+
+        if (m_impl->original_prot & PAGE_READONLY)
+        {
+            m_impl->prot = protection::read;
+        }
+        else if (m_impl->original_prot & PAGE_READWRITE)
+        {
+            m_impl->prot = protection::read | protection::write;
+        }
+        else if (m_impl->original_prot & PAGE_EXECUTE_READWRITE)
+        {
+            m_impl->prot = protection::read | protection::write | protection::execute;
+        }
+
+        return true;
+    }
+
+    bool page::protect(protection prot)
+    {
+        if (VirtualProtect(reinterpret_cast<LPVOID>(start()), size(), impl::translate(prot), nullptr) == 0)
+        {
+            return false;
+        }
+
+        m_impl->prot = prot;
+        return true;
+    }
+
+    std::vector<page> page::pages()
+    {
+        std::vector<page> rtn;
+
+        void *address{};
+        MEMORY_BASIC_INFORMATION info;
+
+        while (VirtualQuery(address, &info, sizeof(info)))
+        {
+            auto base = reinterpret_cast<std::uintptr_t>(info.BaseAddress);
+
+            // NOLINTNEXTLINE(*-optional-access)
+            rtn.emplace_back(std::move(at(base).value()));
+
+            address = reinterpret_cast<void *>(base + info.RegionSize);
+        }
+
+        return rtn;
+    }
+
+    page page::unsafe(std::uintptr_t address)
+    {
+        // NOLINTNEXTLINE(*-optional-access)
+        return at(address).value();
+    }
+
+    std::optional<page> page::at(std::uintptr_t address)
+    {
+        MEMORY_BASIC_INFORMATION info;
+
+        if (VirtualQuery(reinterpret_cast<LPVOID>(address), &info, sizeof(info)) == 0)
+        {
+            return std::nullopt;
+        }
+
+        auto base = reinterpret_cast<std::uintptr_t>(info.BaseAddress);
+
+        page rtn;
+
+        rtn.m_impl->start = base;
+        rtn.m_impl->end = base + info.RegionSize;
+        rtn.m_impl->original_prot = info.Protect;
+
+        if (rtn.m_impl->original_prot & PAGE_READONLY)
+        {
+            rtn.m_impl->prot = protection::read;
+        }
+        else if (rtn.m_impl->original_prot & PAGE_READWRITE)
+        {
+            rtn.m_impl->prot = protection::read | protection::write;
+        }
+        else if (rtn.m_impl->original_prot & PAGE_EXECUTE_READWRITE)
+        {
+            rtn.m_impl->prot = protection::read | protection::write | protection::execute;
+        }
+
+        return rtn;
+    }
+
+    std::shared_ptr<page> page::allocate(std::size_t size, protection prot)
+    {
+        return allocate<alloc_policy::exact>(reinterpret_cast<std::uintptr_t>(nullptr), size, prot);
+    }
+
+    template <>
+    std::shared_ptr<page> page::allocate<alloc_policy::exact>(std::uintptr_t where, std::size_t size,
+                                                              protection protection)
+    {
+        auto *addr = reinterpret_cast<LPVOID>(where);
+        const auto prot = impl::translate(protection);
+        auto *alloc = VirtualAlloc(addr, size, MEM_COMMIT | MEM_RESERVE, prot);
+
+        if (!alloc)
+        {
+            return nullptr;
+        }
+
+        return impl::from(alloc, size, protection);
+    }
+
+    template <>
+    std::shared_ptr<page> page::allocate<alloc_policy::nearby>(std::uintptr_t where, std::size_t size,
+                                                               protection protection)
+    {
+        MEM_EXTENDED_PARAMETER param{};
+        MEM_ADDRESS_REQUIREMENTS requirements{};
+
+        SYSTEM_INFO si{};
+        GetSystemInfo(&si);
+
+        auto granularity = si.dwAllocationGranularity;
+
+        auto min = where - std::numeric_limits<std::int32_t>::max();
+        auto max = where + std::numeric_limits<std::int32_t>::max();
+
+        min += (granularity - (min % granularity));
+        max -= (max % granularity) + 1;
+
+        requirements.Alignment = 0;
+        requirements.HighestEndingAddress = reinterpret_cast<LPVOID>(max);
+        requirements.LowestStartingAddress = min < 0 ? nullptr : reinterpret_cast<LPVOID>(min);
+
+        param.Pointer = &requirements;
+        param.Type = MemExtendedParameterAddressRequirements;
+
+        static auto VirtualAlloc2 = []() {
+            auto *kernel_base = LoadLibraryA("kernelbase.dll");
+            return reinterpret_cast<decltype(::VirtualAlloc2) *>(GetProcAddress(kernel_base, "VirtualAlloc2"));
+        }();
+
+        auto *alloc = VirtualAlloc2(GetCurrentProcess(), nullptr, size, MEM_COMMIT | MEM_RESERVE,
+                                    impl::translate(protection), &param, 1);
+
+        if (!alloc)
+        {
+            return nullptr;
+        }
+
+        return impl::from(alloc, size, protection);
+    }
+
+    DWORD page::impl::translate(protection prot)
+    {
+        DWORD rtn{0};
+
+        if (prot & protection::read)
+        {
+            rtn = PAGE_READONLY;
+        }
+        if (prot & protection::write)
+        {
+            rtn = PAGE_READWRITE;
+        }
+        if (prot & protection::execute)
+        {
+            rtn = PAGE_EXECUTE_READWRITE;
+        }
+
+        return rtn;
+    }
+
+    std::shared_ptr<page> page::impl::from(LPVOID address, std::size_t size, protection protection)
+    {
+        auto *rtn = new page;
+
+        rtn->m_impl->start = reinterpret_cast<std::uintptr_t>(address);
+        rtn->m_impl->end = rtn->m_impl->start + size;
+
+        rtn->m_impl->original_prot = translate(protection);
+        rtn->m_impl->prot = protection;
+
+        auto deleter = [](page *page) {
+            VirtualFree(reinterpret_cast<LPVOID>(page->start()), page->size(), MEM_RELEASE);
+            delete page;
+        };
+
+        return {rtn, deleter};
+    }
+} // namespace lime
