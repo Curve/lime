24bce5cf917397759183a33f87d954a40a731750
Curve <fynnbwdt@gmail.com>
2023-12-03 12:35:25 +0100

    refactor: use boost::ut for tests

--- tests/hook.test.cpp
+++ tests/hook.test.cpp
@@ -1,33 +1,39 @@
-#include <catch2/catch_test_macros.hpp>
 #include <lime/hooks/hook.hpp>
+#include <boost/ut.hpp>
 
-int test(int param)
+using namespace boost::ut;
+using namespace boost::ut::literals;
+
+int test_fn(int param)
 {
     return param;
 }
 
 std::unique_ptr<lime::hook<int(int)>> original_test;
+
 int test_hook(int param)
 {
     return original_test->original()(param + 5);
 }
 
-TEST_CASE("Hooks are tested", "[hook]")
+suite webview_suite = []
 {
-    REQUIRE(test(10) == 10);
+    expect(eq(test_fn(10), 10));
 
-    original_test = std::move(lime::hook<int(int)>::create(test, test_hook).value());
-    REQUIRE(test(10) == 15);
+    original_test = std::move(lime::hook<int(int)>::create(test_fn, test_hook).value());
+    expect(eq(test_fn(10), 15));
 
     original_test.reset();
-    REQUIRE(test(10) == 10);
+    expect(eq(test_fn(10), 10));
 
-    lime::hook<int(int)>::create(test, [](auto *hook, int param) -> int {
-        auto rtn = hook->original()(param + 10);
-        delete hook;
-        return rtn;
-    });
+    lime::hook<int(int)>::create(test_fn,
+                                 [](auto *hook, int param) -> int
+                                 {
+                                     auto rtn = hook->original()(param + 10);
+                                     delete hook;
+                                     return rtn;
+                                 });
 
-    REQUIRE(test(10) == 20);
-    REQUIRE(test(10) == 10);
-}
+    expect(eq(test_fn(10), 20));
+    expect(eq(test_fn(10), 10));
+};
---
+++ tests/main.cpp
@@ -0,0 +1,6 @@
+#include <boost/ut.hpp>
+
+int main()
+{
+    return boost::ut::cfg<boost::ut::override>.run();
+}
