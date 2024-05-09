ae2265ad7242c73c1555ce1023fc116b57e49f58
Curve <fynnbwdt@gmail.com>
2024-05-09 02:18:34 +0200

    refacror(tests/instruction): use shell code [skip ci]

--- tests/instruction.test.cpp
+++ tests/instruction.test.cpp
@@ -1,75 +1,100 @@
 #include <boost/ut.hpp>
+
+#include <cmath>
 #include <lime/instruction.hpp>
 
 using namespace boost::ut;
 using namespace boost::ut::literals;
 
-int func2(int a, int b)
-{
-    return a + b;
-}
-
-int func1(int a)
-{
-    if (a > 10)
-    {
-        return 20;
-    }
-
-    return func2(a, 30);
-}
+std::vector<unsigned char> code = {
+#if INTPTR_MAX == INT64_MAX
+    0x8b, 0x02,                               // mov eax, [rdx]
+    0x4c, 0x8b, 0xc1,                         // mov [rcx], eax
+    0x48, 0x8B, 0x05, 0x05, 0x00, 0x00, 0x00, // mov rax, [rip+0x5]
+    0xff, 0x25, 0x10, 0x00, 0x00, 0x00,       // jmp [rip+0x10]
+    0xff, 0xe0,                               // jmp rax
+#else
+    0x8B, 0x02,                         // mov eax, [edx]
+    0x89, 0x01,                         // mov [ecx], eax
+    0xA1, 0x05, 0x00, 0x00, 0x00,       // mov eax, [rip+0x5]
+    0xFF, 0x25, 0x10, 0x00, 0x00, 0x00, // jmp [rip+0x10]
+    0xFF, 0xE0,                         // jmp eax
+#endif
+};
 
 suite<"Instruction"> instruction_suite = []
 {
-    auto instruction = lime::instruction::at(reinterpret_cast<std::uintptr_t>(func1));
+    static constexpr auto MNEMONIC_MOV = 436;
+    static constexpr auto MNEMONIC_JMP = 311;
+
+    auto *data = code.data();
+
+    auto instruction = lime::instruction::at(reinterpret_cast<std::uintptr_t>(data));
 
     expect(eq(instruction.has_value(), true));
-    expect(eq(instruction->addr(), reinterpret_cast<std::uintptr_t>(func1)));
 
-    static constexpr auto MNEMONIC_JLE = 306;
-    auto jump                          = instruction->next(MNEMONIC_JLE);
+    expect(eq(instruction->size(), 2));
+    expect(eq(instruction->addr(), reinterpret_cast<std::uintptr_t>(data)));
 
-    expect(eq(jump.has_value(), true));
-    expect(eq(jump->relative(), true));
-    expect(eq(jump->branching(), true));
+    expect(eq(instruction->relative(), false));
+    expect(eq(instruction->branching(), false));
+    expect(eq(instruction->mnemonic(), MNEMONIC_MOV));
 
-    expect(gt(jump->size(), 0));
-    expect(eq(jump->mnemonic(), MNEMONIC_JLE));
-    expect(gt(jump->addr(), instruction->addr()));
+    auto mov = instruction->next();
 
-    static constexpr auto MNEMONIC_CALL = 67;
-    auto call                           = instruction->next(MNEMONIC_CALL);
+    expect(eq(mov.has_value(), true));
 
-    expect(eq(call.has_value(), true));
-    expect(eq(call->relative(), true));
+    expect(eq(mov->relative(), false));
+    expect(eq(mov->branching(), false));
+    expect(eq(mov->mnemonic(), MNEMONIC_MOV));
 
-    expect(gt(call->size(), 0));
-    expect(eq(call->mnemonic(), MNEMONIC_CALL));
-    expect(gt(call->addr(), instruction->addr()));
-
-    /*
-     * We depend on compiler generated assembly here which isn't always what we'd expect it to be.
-     * This test would probably be better suited on a small shell-code (TODO).
-     *
-     * As clang and gcc both compile `func1` and `func2` into roughly what we'd expect while MSVC does not the following
-     * asserts are only enabled for Windows under clang-cl
-     */
-
-#if !defined(_MSC_VER) || defined(__clang__)
-    auto follow = call->follow();
-
-    expect(eq(follow.has_value(), true));
-    expect(eq(follow->addr(), reinterpret_cast<std::uintptr_t>(func2)));
-
-    auto next = follow->next();
-
-    expect(eq(next.has_value(), true));
-    expect(gt(next->addr(), follow->addr()));
-
-    auto prev = next->prev();
+    auto prev = mov->prev();
 
     expect(eq(prev.has_value(), true));
-    expect(eq(prev->addr(), follow->addr()));
-    expect(eq(prev->mnemonic(), follow->mnemonic()));
+    expect(eq(prev->mnemonic(), MNEMONIC_MOV));
+
+    expect(eq(prev->size(), 2));
+    expect(eq(prev->addr(), reinterpret_cast<std::uintptr_t>(data)));
+
+    auto rel_move = mov->next();
+
+    expect(eq(rel_move.has_value(), true));
+
+    expect(eq(rel_move->relative(), true));
+    expect(eq(rel_move->branching(), false));
+    expect(eq(rel_move->mnemonic(), MNEMONIC_MOV));
+
+    auto jmp       = rel_move->next();
+    auto jmp_until = instruction->next(MNEMONIC_JMP);
+
+    expect(eq(jmp.has_value(), true));
+    expect(eq(jmp_until.has_value(), true));
+
+    expect(eq(jmp->addr(), jmp_until->addr()));
+
+    expect(eq(jmp->relative(), true));
+    expect(eq(jmp->branching(), true));
+    expect(eq(jmp->mnemonic(), MNEMONIC_JMP));
+
+    auto abs_jmp = jmp->next();
+
+    expect(eq(abs_jmp.has_value(), true));
+
+    expect(eq(abs_jmp->relative(), false));
+    expect(eq(abs_jmp->branching(), true));
+    expect(eq(abs_jmp->mnemonic(), MNEMONIC_JMP));
+
+#if INTPTR_MAX == INT64_MAX
+    expect(eq(next->size(), 3));
+    expect(eq(next->addr(), reinterpret_cast<std::uintptr_t>(data) + 0x2));
+
+    expect(eq(rel_move->size(), 7));
+    expect(eq(rel_move->addr(), reinterpret_cast<std::uintptr_t>(data) + 15));
+
+    expect(eq(jmp->size(), 6));
+    expect(eq(jmp->addr(), rel_move->addr() + rel_move->size()));
+
+    expect(eq(abs_jmp->size(), 2));
+    expect(eq(abs_jmp->addr(), jmp->addr() + jmp->size()));
 #endif
 };
