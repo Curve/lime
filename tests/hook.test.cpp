3a106b54fbd778c50471c48fc8c91c7fd9a32d84
Curve <fynnbwdt@gmail.com>
2026-04-11 19:30:49 +0200

    refactor(tests/hook): cleanup

--- tests/hook.test.cpp
+++ tests/hook.test.cpp
@@ -25,31 +25,71 @@ int test_hook(int param)
     return original_test->original()(param + 5);
 }
 
+#ifdef __GNUC__
+template <typename T>
+inline void dont_optimize(T const &value)
+{
+    asm volatile("" : : "g"(value) : "memory");
+}
+#endif
+
 suite<"Hooks"> hook_suite = []
 {
-    expect(eq(test_fn(10), 10));
-    expect(eq(test_fn(1337), 0));
+    using test_fn_t                 = int (*)(int);
+    volatile auto avoidOptimization = true;
+    auto func                       = test_fn_t{};
 
-    original_test.emplace(*lime::make_hook(test_fn, test_hook));
-    expect(eq(test_fn(10), 15));
-    expect(eq(test_fn(1337), 1342));
+    if (avoidOptimization)
+    {
+        func = test_fn;
+    }
+    else
+    {
+        func = reinterpret_cast<test_fn_t>(0xDEADBEEF);
+    }
+
+#ifdef __GNUC__
+    dont_optimize(func);
+#endif
+
+    expect(eq(func(10), 10));
+    expect(eq(func(1337), 0));
+
+    if (auto hook = lime::make_hook(func, test_hook); hook.has_value())
+    {
+        original_test.emplace(*std::move(hook));
+    }
+    else
+    {
+        expect(false);
+    }
+
+    expect(eq(func(10), 15));
+    expect(eq(func(1337), 1342));
 
     original_test.reset();
-    expect(eq(test_fn(10), 10));
+    expect(eq(func(10), 10));
 
-    original_test.emplace(*lime::make_hook(test_fn, test_hook));
-    expect(eq(test_fn(10), 15));
+    if (auto hook = lime::make_hook(func, test_hook); hook.has_value())
+    {
+        original_test.emplace(*std::move(hook));
+    }
+    else
+    {
+        expect(false);
+    }
 
+    expect(eq(func(10), 15));
     original_test.reset();
-    expect(eq(test_fn(10), 10));
+    expect(eq(func(10), 10));
 
-    lime::make_hook(test_fn, [](auto &hook, int param) { return std::move(hook).reset()(param + 5); });
+    expect(lime::make_hook(func, [](auto &hook, int param) { return std::move(hook).reset()(param + 5); }).has_value());
 
-    expect(eq(test_fn(10), 15));
-    expect(eq(test_fn(10), 10));
+    expect(eq(func(10), 15));
+    expect(eq(func(10), 10));
 
-    lime::make_hook(test_fn, [](auto &hook, int param) { return std::move(hook).reset()(param + 10); });
+    expect(lime::make_hook(func, [](auto &hook, int param) { return std::move(hook).reset()(param + 10); }).has_value());
 
-    expect(eq(test_fn(10), 20));
-    expect(eq(test_fn(10), 10));
+    expect(eq(func(10), 20));
+    expect(eq(func(10), 10));
 };
